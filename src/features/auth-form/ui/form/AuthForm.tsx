45e27f895c643497449c9f5893f75b34288d0c6b
s.saltykov <fermager@yandex.ru>
2025-03-04 16:55:16 +0700

    fixed

--- src/features/auth-form/ui/form/AuthForm.tsx
+++ src/features/auth-form/ui/form/AuthForm.tsx
@@ -11,16 +11,18 @@ import style from './styled.module.scss';
 
 const AuthForm: FC = () => {
   return (
-    <div className={style.container}>
-      <h2 className={style.header}>Регистрация</h2>
-      <Input required id="name" placeholder="Имя" />
-      <Select options={country} defaultValue={country[0].value} id="country" />
-      <Textarea id="description" placeholder="Коротко о себе" />
-      <Button onClick={() => {
-      }}>Continue</Button>
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <h2 className={style.header}>Регистрация</h2>
+        <Input required id="name" placeholder="Имя" />
+        <Select options={country} defaultValue={country[0].value} id="country" />
+        <Textarea id="description" placeholder="Коротко о себе" />
+        <Button onClick={() => {
+          }}>Continue</Button>
+      </div>
     </div>
   );
 };
 
-export default AuthForm
+export default AuthForm;
 
--- src/features/data-table/ui/table/DataTable.tsx
+++ src/features/data-table/ui/table/DataTable.tsx
@@ -1,16 +1,21 @@
 import { FC } from 'react';
 
-// import style from './styled.module.scss';
 import { columns, response } from '../constants';
 import { Table } from '../../../../shared/ui';
+import style from './styled.module.scss';
 
 const DataTable: FC = () => {
   return (
-    <div>
-      <Table data={response} columns={columns}/>
+    <div className={style.container}>
+      <h1 className={style.header}>Таблица</h1>
+      <h3 className={style.description}>
+        с вертикальным и горизонтальным скролом,
+      закрепленной верхней строкой и первой колонкой
+      </h3>
+      <Table data={response} columns={columns} />
     </div>
   );
 };
 
-export default DataTable
+export default DataTable;
 
